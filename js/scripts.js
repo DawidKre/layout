3f4e48312af64e7c4e086d9703ea581579832fe4
dawid <bliksem031@gmail.com>
2017-01-26 17:17:41 +0100

    Image

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,74 @@
 $(function () {
+
+    // $.site('disable console'); // disables window.console functionality
+    // $.site('enable console'); // re-enables window.console functionality
+
+// enable all debug
+    $.site('enable debug');
+
+// disable all debug
+    $.site('enable debug');
+
+
+    // normalizes console and requestAnimationFrame DOM methods
+    $.site('normalize');
+    
+
+    /*
+    $.fn.checkbox.settings = {
+        debug:true
+    };
+    $.fn.dropdown.settings = {
+        debug:true
+    };*/
+
+
+    phoneAdd = $('#phone-add');
+    offerAdd = $('#offer-add');
+    billingEdit = $('#billing-edit');
+    emailEdit = $('#email-edit');
+    passwordEdit = $('#password-edit');
+
+    
+    billingEdit.click(function () {
+        $('.billing-edit')
+            .modal('show')
+        ;
+    });
+
+    emailEdit.click(function () {
+        $('.email-edit')
+            .modal('show')
+        ;
+    });
+
+    passwordEdit.click(function () {
+        $('.password-edit')
+            .modal('show')
+        ;
+    });
+
+    phoneAdd.click(function () {
+        $('.phone-add')
+            .modal('show')
+        ;
+    });
+
+    $('.ui.marks-checkbox').dropdown({
+        action: 'nothing'
+    });
+    $('.ui.checkbox').checkbox();
+
+    
+    offerAdd.click(function () {
+        $('.offer-add')
+            .modal('show')
+        ;
+    });
+    
+    $('.ui.accordion').accordion('refresh');
+    
+    $('.ui.dropdown').dropdown();
     var items = $('#v-nav>ul>li').each(function () {
         $(this).click(function () {
             //remove previous class and add it to clicked tab
@@ -6,28 +76,49 @@ $(function () {
             $(this).addClass('current');
 
             //hide all content divs and show current one
+            
             $('#v-nav>div.tab-content').hide().eq(items.index($(this))).show();
 
             window.location.hash = $(this).attr('tab');
+            
         });
     });
 
     if (location.hash) {
-        showTab(location.hash);
+        tab = location.hash.substring(1);
+        counter = tab.substring(3)-1;
+        
+        showTab(tab, counter);
     }
     else {
-        showTab("tab1");
+        showTab("tab1",0);
     }
 
-    function showTab(tab) {
-        $("#v-nav ul li:[tab*=" + tab + "]").click();
+    function showTab(tab, counter) {
+        var tabContents = $('#v-nav>div.tab-content');
+        
+        $("[tab="+tab+"]").addClass('current');
+        tabContents.hide();
+        tabContents.eq(counter).show();
     }
+    
 
     // Bind the event hashchange, using jquery-hashchange-plugin
-    $(window).hashchange(function () {
-        showTab(location.hash.replace("#", ""));
-    })
+    // $(window).hashchange(function () {
+    //     showTab(location.hash.replace("#", ""));
+    // });
 
     // Trigger the event hashchange on page load, using jquery-hashchange-plugin
-    $(window).hashchange();
+    // $(window).hashchange();
+    
+
 });
+/*
+
+function showModal() {
+    $("#billing-edit").click(function () {
+        $('.ui.modal')
+            .modal('show')
+        ;
+    });
+}*/
---
+++ js/validation.js
@@ -0,0 +1,107 @@
+$(function () {
+
+    $('#offer-form')
+        .form({
+            fields: {
+                name: {
+                    identifier: 'name',
+                    rules: [
+                        {
+                            type   : 'empty',
+                            prompt : 'Podaj nazwe oferty'
+                        }
+                    ]
+                },
+                marks: {
+                    identifier: 'marks',
+                    rules: [
+                        {
+                            type   : 'minCount[2]',
+                            prompt : 'Wybierz conajmniej dwie marki'
+                        }
+                    ]
+                },
+                phone: {
+                    identifier: 'phone',
+                    rules: [
+                        {
+                            type   : 'empty',
+                            prompt : 'Podaj numer telefonu'
+                        }
+                    ]
+                },
+                adress: {
+                    identifier: 'adress',
+                    rules: [
+                        {
+                            type   : 'empty',
+                            prompt : 'Podaj adres'
+                        }
+                    ]
+                },
+                adressCode: {
+                    identifier: 'adress-code',
+                    rules: [
+                        {
+                            type   : 'empty',
+                            prompt : 'Podaj adres'
+                        },
+                        {
+                            type   : 'number',
+                            prompt : 'Zły format liczby'
+                        }
+                    ]
+                },
+                localization: {
+                    identifier: 'localization',
+                    rules: [
+                        {
+                            type   : 'empty',
+                            prompt : 'Wybierz lokalizacje'
+                        }
+                    ]
+                },
+                zip: {
+                    identifier: 'zip',
+                    rules: [
+                        {
+                            type   : 'empty',
+                            prompt : 'Wpisz kod pocztowy'
+                        }
+                    ]
+                },
+                city: {
+                    identifier: 'city',
+                    rules: [
+                        {
+                            type   : 'empty',
+                            prompt : 'Wpisz miejscowość'
+                        }
+                    ]
+                },
+                categories: {
+                    identifier: 'categories',
+                    rules: [
+                        {
+                            type   : 'minCount[1]',
+                            prompt : 'Wybierz conajmniej jedną kategorie'
+                        }
+                    ]
+                },
+                description: {
+                    identifier: 'description',
+                    rules: [
+                        {
+                            type   : 'empty',
+                            prompt : 'Wpisz opis firmy'
+                        }
+                    ]
+                }
+                
+            },
+            
+            inline : true,
+            on     : 'submit'
+        })
+    ;
+});
